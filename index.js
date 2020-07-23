2c9e3efe0b50a5e7e3323cdb02ec40710c8cac9f
jlorenc1 <joshua.lorenc@icloud.com>
2020-07-23 09:55:39 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  const find = array.find(year => year.result === "W")
+  return !!find ? find.year : undefined
+}
